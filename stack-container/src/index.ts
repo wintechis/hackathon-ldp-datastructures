46911e587a226f0c19d3a11e07fcadaaf3d452c5
Thomas Wehr <derwehr@gmail.com>
2024-09-13 18:52:09 +0200

    fix `GET` operations returning JSON instead of turtle

--- stack-container/src/index.ts
+++ stack-container/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import process from 'process';
-import { Quad, Writer, DataFactory, NamedNode, Parser, StreamParser } from 'n3';
+import { Quad, Writer, DataFactory, NamedNode, StreamParser } from 'n3';
 
-const { quad, namedNode, literal, blankNode } = DataFactory;
+const { quad, namedNode, blankNode } = DataFactory;
 const app = express();
 
 const port: number = parseInt(process.argv[2]);
@@ -12,13 +12,13 @@ const LDP = 'http://www.w3.org/ns/ldp#';
 const SC = 'https://solid.ti.rw.fau.de/public/ns/stream-containers#';
 
 const map: Map<string, Quad[]> = new Map();
-let ToS : string;
+let ToS : string | undefined = undefined;
 
 var router = express.Router();
 router.route('/').get(function (req, res, next) {
 	res.setHeader('Content-Type', 'text/turtle');
 	res.status(200);
-	let writer: Writer<Quad> = new Writer(res);
+	const writer = new Writer({ format: 'Turtle' });
 	let window = blankNode();
 	let nextNode : NamedNode;
 	if (ToS === undefined) {
@@ -33,7 +33,14 @@ router.route('/').get(function (req, res, next) {
 		quad(window, namedNode(LDP + 'next'), nextNode),
 	]);
 	Array.from(map.keys()).forEach((containedResource) => writer.addQuad(namedNode(''), namedNode(LDP + 'contains'), namedNode(containedResource)));
-	writer.end();
+	
+	writer.end((error, result) => {
+        if (error) {
+            next(error);
+        } else {
+            res.send(result);
+        }
+    });
 });
 // push a new quad onto the stack
 router.route('/push').post(function (req, res, next) {
@@ -65,47 +72,70 @@ router.route('/push').post(function (req, res, next) {
 
 // pop a quad from the stack
 router.route('/pop').post(function (req, res, next) {
-	if(ToS === undefined) {
-		res.status(200);
-		res.send(namedNode(LDP+'nil'));
-	} else {
-		// remove top of stack
-		let popNode = map.get(ToS);
-		if (popNode === undefined){
-			res.status(200);
-			res.send(namedNode(LDP+'nil'));
-			return;
-		}
+	res.setHeader('Content-Type', 'text/turtle');
+	res.status(200);
+    const writer = new Writer({ format: 'Turtle' });
+    if (ToS === undefined) {
+        writer.addQuad(namedNode(LDP + 'nil'), namedNode('rdf:type'), namedNode('rdf:nil'));
+    } else {
+        const quads = map.get(ToS) ?? [];
+        writer.addQuads(quads);
+
 		// remove quad from map
 		map.delete(ToS);
 
 		// set new top of stack
-		let newToS = popNode.filter(q => q.predicate.equals(namedNode(LDP + 'next')))[0].object.value;
-		ToS = newToS;
-
-		res.status(200);
-		res.send(popNode);
+		let newToS = quads.find(quad => quad.predicate.value === LDP + 'next')?.object.value;
+		if (newToS === LDP + 'nil') {
+			ToS = undefined;
+		} else {
+			ToS = newToS;
+		}
 	}
+
+	writer.end((error, result) => {
+        if (error) {
+            next(error);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 //peak ToS
 router.route('/peak').get(function (req, res, next) {
 	res.setHeader('Content-Type', 'text/turtle');
 	res.status(200);
-	if(ToS === undefined) {
-		res.send(namedNode(LDP+'nil'));
-	} else {
-		res.send(map.get(ToS));
-	}
+    const writer = new Writer({ format: 'Turtle' });
+    if (ToS === undefined) {
+        writer.addQuad(namedNode(LDP + 'nil'), namedNode('rdf:type'), namedNode('rdf:nil'));
+    } else {
+        const quads = map.get(ToS) ?? [];
+        writer.addQuads(quads);
+    }
+    writer.end((error, result) => {
+        if (error) {
+            next(error);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 router.route('/*').get(function (req, res, next) {
 	res.setHeader('Content-Type', 'text/turtle');
 	res.status(200);
-	let writer: Writer<Quad> = new Writer(res);
+	let writer: Writer<Quad> = new Writer({ format: 'Turtle' });
 	writer.addQuads(map.get(req.path) ?? []);
-	writer.end();
+    writer.end((error, result) => {
+        if (error) {
+            next(error);
+        } else {
+            res.send(result);
+        }
+    });
 });
+
 app.use(router);
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
